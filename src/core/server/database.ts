abc7cead45fc5449b5c67787bfab7c253bd531e0
Remco Stoeten <remcostoeten@hotmail.com>
2024-08-02 20:15:11 +0200

    Add drizzle client.

---
+++ src/core/server/database.ts
@@ -0,0 +1,11 @@
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+
+const client = createClient({
+    url: process.env.DATABASE_URL,
+    authToken: process.env.DATABASE_AUTH_TOKEN
+});
+
+const db = drizzle(client);
+
+export default db;
