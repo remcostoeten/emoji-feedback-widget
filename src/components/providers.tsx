af220ebc2ab928b20cf5a3f7d6614b19b769bc61
Remco Stoeten <remcostoeten@hotmail.com>
2024-08-06 22:41:40 +0200

    Minor json version changes.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import React from 'react'
 import { Toaster } from 'sonner'
 import { I18nextProvider } from 'react-i18next'
@@ -11,9 +9,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<I18nextProvider i18n={i18n}>
 			{children}
-			<SpeedInsights />
 			<Toaster position="top-right" />
-			<Analytics />
 		</I18nextProvider>
 	)
 }
